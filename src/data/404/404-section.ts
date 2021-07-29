f87a194c6187ebd7a6e61dc6e1776c36fb27d04b
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-07-30 04:00:28 +0900

    add: 404 Page

---
+++ src/data/404/404-section.ts
@@ -0,0 +1,8 @@
+import { SectionItem } from '../../types/section-item'
+
+export const NotFoundSection: SectionItem[] = [
+  {
+    title: '404 Not Found',
+    text: ['ページが見つかりませんでした 😿']
+  }
+]
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,16 @@
-import * as React from 'react'
+import { NotFoundSection } from '../data/404/404-section'
+import React from 'react'
+import Sections from '../components/common/sections'
+import Seo from '../components/common/seo'
+import Title from '../components/common/title'
+import Layout from '../components/layout'
 
-const NotFoundPage = (): JSX.Element => {
-  return (
-    <main>
-      <title>Not found</title>
-      <h1>Page not found</h1>
-    </main>
-  )
-}
+const NotFoundPage = (): JSX.Element => (
+  <Layout>
+    <Seo title="404 Not Found" />
+    <Title name="Opps!" />
+    <Sections items={NotFoundSection} />
+  </Layout>
+)
 
 export default NotFoundPage
