604bee9a20255e3e0be71648fdb4838b48dd0e95
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-07-30 03:15:36 +0900

    add: plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,44 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: 'https://www.yourdomain.tld',
-    title: 'arrow2nd-portfolio'
+    title: 'arrow2nd',
+    titleTemplate: `%s | arrow2nd`,
+    description: 'あろーがつくったものをまとめたサイト',
+    author: 'arrow2nd',
+    url: '',
+    image: `/icons/icon-256x256.png`,
+    twitterUsername: '@arrow_2nd'
   },
   plugins: [
     'gatsby-plugin-typescript',
     'gatsby-plugin-postcss',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images`
+      }
+    },
     {
       resolve: 'gatsby-plugin-google-fonts',
       options: {
         fonts: ['M PLUS Rounded 1c'],
         display: 'swap'
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'arrow2nd',
+        short_name: 'arrow2nd',
+        start_url: '/',
+        background_color: '#faf8f7',
+        theme_color: '#faf8f7',
+        display: 'minimal-ui',
+        icon: `${__dirname}/src/images/arrow2nd.png`
       }
     }
   ]
