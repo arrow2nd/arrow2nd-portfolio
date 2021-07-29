bb91fa1b343e47ce995b4225a58d3abcf3bf25bf
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-07-30 03:16:20 +0900

    add

---
+++ src/components/common/seo.tsx
@@ -0,0 +1,83 @@
+/**
+ * SEO component that queries for data with
+ *  Gatsby's useStaticQuery React hook
+ *
+ * See: https://www.gatsbyjs.com/docs/use-static-query/
+ */
+
+import { Helmet } from 'react-helmet'
+import { useLocation } from '@reach/router'
+import { useStaticQuery, graphql } from 'gatsby'
+import * as React from 'react'
+
+type Props = {
+  title?: string
+  desc?: string
+  image?: string
+  article?: boolean
+}
+
+const Seo = ({
+  title = '',
+  desc = '',
+  image = '',
+  article = false
+}: Props): JSX.Element => {
+  const { pathname } = useLocation()
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            defaultTitle: title
+            titleTemplate
+            defaultDescription: description
+            originUrl: url
+            defaultImage: image
+            twitterUsername
+          }
+        }
+      }
+    `
+  )
+
+  const {
+    defaultTitle,
+    titleTemplate,
+    defaultDescription,
+    originUrl,
+    defaultImage,
+    twitterUsername
+  } = site.siteMetadata
+
+  // URL末尾のスラッシュを削除
+  const siteUrl = originUrl.slice(0, -1)
+
+  const seo = {
+    title: title || defaultTitle,
+    ogpTitle: title ? titleTemplate.replace(/%s/, title) : defaultTitle,
+    description: desc || defaultDescription,
+    image: `${siteUrl}${image || defaultImage}`,
+    url: `${siteUrl}${pathname}`
+  }
+
+  return (
+    <Helmet title={seo.title} titleTemplate={title ? titleTemplate : ''}>
+      <html lang="ja" />
+      <meta name="description" content={seo.description} />
+      <meta name="image" content={seo.image} />
+      <meta property="og:url" content={seo.url} />
+      {(article ? true : null) && <meta property="og:type" content="article" />}
+      <meta property="og:title" content={seo.ogpTitle} />
+      <meta property="og:description" content={seo.description} />
+      <meta property="og:image" content={seo.image} />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:creator" content={twitterUsername} />
+      <meta name="twitter:title" content={seo.ogpTitle} />
+      <meta name="twitter:description" content={seo.description} />
+      <meta name="twitter:image" content={seo.image} />
+    </Helmet>
+  )
+}
+
+export default Seo
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,15 @@ module.exports = {
     extend: {
       colors: {
         'natural-white': '#faf8f7',
-        'natural-black': '#1c1c1c'
+        'natural-black': '#1c1c1c',
+        'natural-gray': '#464646',
+        arrow2nd: '#5B94FF',
+        twitter: '#1DA1F2',
+        mastodon: '#2B90D9',
+        github: '#333333'
+      },
+      letterSpacing: {
+        super: '.25rem'
       }
     }
   },
