52f3e1314431eaf1d3ff6a88c79f6e081312371d
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-12-02 14:07:35 +0900

    add: page transition animation

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -6,14 +6,14 @@ type Props = {
   children: React.ReactNode
 }
 
-const Layout = ({ children }: Props): JSX.Element => {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <main className="flex-1 mx-4 md:mx-8 mt-16">{children}</main>
-      <Footer />
-    </div>
-  )
-}
+const Layout = ({ children }: Props): JSX.Element => (
+  <div className="flex flex-col min-h-screen">
+    <Header />
+    <main className="flex-1 mx-4 md:mx-8 mt-16 transition-anim">
+      {children}
+    </main>
+    <Footer />
+  </div>
+)
 
 export default Layout
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,23 +4,22 @@ import Seo from '../components/common/seo'
 import LinkButton from '../components/top/link-button'
 import { StaticImage } from 'gatsby-plugin-image'
 
-const Index = (): JSX.Element => {
-  return (
-    <div className="flex flex-col h-screen justify-center items-center">
-      <Seo />
-      <StaticImage
-        className="border border-gray-200 rounded-full"
-        src="../images/arrow2nd.png"
-        alt="arrow2nd"
-        width={200}
-      />
-      <span className="mt-6 text-3xl text-natural-gray tracking-super">
-        arrow2nd
-      </span>
-      <Accounts className="mt-6" />
-      <LinkButton className="mt-6" />
-    </div>
-  )
-}
+const Index = (): JSX.Element => (
+  <div className="flex flex-col h-screen justify-center items-center transition-anim">
+    <Seo />
+    <StaticImage
+      className="border border-gray-200 rounded-full"
+      src="../images/arrow2nd.png"
+      alt="arrow2nd"
+      width={200}
+      placeholder="dominantColor"
+    />
+    <span className="mt-6 text-3xl text-natural-gray tracking-super">
+      arrow2nd
+    </span>
+    <Accounts className="mt-6" />
+    <LinkButton className="mt-6" />
+  </div>
+)
 
 export default Index
